2a72a10b411063408c347e469e545d9ff82aa1ce
Blake Guilloud <fbguillo@gmail.com>
2016-12-30 15:41:34 -0500

    More debug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // const pinRoutes = require('./lib/routes/pin.routes');
-// const apiRoutes = require('./lib/routes/api.routes');
+const apiRoutes = require('./lib/routes/api.routes');
 
 const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
 const port = process.env.PORT || 8100;
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
-//
+
 // app.use('/', pinRoutes);
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(port, () => console.log('app listening on port: ', port));
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-import { getApiKey } from '../controllers/api.controller.js';
-
-router.get('/', getApiKey);
+// import { getApiKey } from '../controllers/api.controller.js';
+//
+// router.get('/', getApiKey);
 
 module.exports = router;
