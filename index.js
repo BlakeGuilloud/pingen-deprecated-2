9018f6a5529dc59a9dd588468d5f227431cf3616
Blake Guilloud <fbguillo@gmail.com>
2016-12-30 15:37:46 -0500

    checking pin routes

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const pinRoutes = require('./lib/routes/pin.routes');
-const apiRoutes = require('./lib/routes/api.routes');
+const pinRoutes = require('./lib/routes/pin.routes');
+// const apiRoutes = require('./lib/routes/api.routes');
 
 const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
 const port = process.env.PORT || 8100;
 
-// mongoose.connect(db);
+mongoose.connect(db);
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-//
-// app.use(bodyParser.json());
-//
-// app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
 //
 // app.use('/', pinRoutes);
 // app.use('/api', apiRoutes);
