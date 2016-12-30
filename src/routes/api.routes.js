fd524c285804729d59ff773977578101918fabeb
Blake Guilloud <fbguillo@gmail.com>
2016-12-30 15:46:24 -0500

    I dont believe you, heroku

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,16 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-// import { getApiKey } from '../controllers/api.controller.js';
-//
-// router.get('/', (getApiKey));
-router.get('/', (req, res) => {
-  res.status(401).json({
-    code: 401,
-    message: 'Invalid API key',
-  });
+import { getApiKey } from '../controllers/api.controller.js';
 
-  return false;
-});
+router.get('/', (getApiKey));
 
 module.exports = router;
