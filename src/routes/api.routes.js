8952cd4e6afd1015f6061ffcab06c47e1ad58acf
Blake Guilloud <fbguillo@gmail.com>
2016-12-30 15:43:30 -0500

    debug

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 // import { getApiKey } from '../controllers/api.controller.js';
 //
-// router.get('/', getApiKey);
+// router.get('/', (getApiKey));
+router.get('/', (req, res) => {
+  res.status(401).json({
+    code: 401,
+    message: 'Invalid API key',
+  });
+
+  return false;
+});
 
 module.exports = router;
