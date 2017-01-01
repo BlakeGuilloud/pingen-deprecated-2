7e00a2af840a408fa5a181e4b9355c478cafe371
Blake Guilloud <fbguillo@gmail.com>
2017-01-01 12:06:27 -0500

    build

--- src/helpers/pin.helpers.js
+++ src/helpers/pin.helpers.js
@@ -1,25 +1,26 @@
 import * as PinService from '../services/pin.service';
 
-export function generatePin (shuttle) {
+export async function generatePin (shuttle) {
   const newShuttle = shuttle;
   const pinLength = newShuttle.data.pinLength || 1000000;
 
-  newShuttle.data.pin = createPin(pinLength);
+  newShuttle.data.pin = await createPin(pinLength);
 
   return Promise.resolve(newShuttle);
 }
 
 export function markToDestroy(shuttle) {
   const newShuttle = shuttle;
+  const pinId = newShuttle.params.id;
 
   const timeout = newShuttle.data.timeout || 30000;
 
-  setTimeout(() => PinService.destroyPin(newShuttle.params.id), timeout);
+  setTimeout(() => PinService.destroyPin(pinId), timeout);
 
   return Promise.resolve(newShuttle);
 }
 
-function createPin (pinLength) {
+async function createPin (pinLength) {
   const pin = Math.floor(Math.random() * pinLength);
 
   if (pin.toString()[0] == '0') {
