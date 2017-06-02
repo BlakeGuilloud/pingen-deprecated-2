1aa99147a6b0815dcb06685992f04b05e929f178
Forrest Guilloud <fbguillo@gmail.com>
2017-06-01 22:16:15 -0400

    scrapped project

--- src/models/api.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-const ApiSchema = new Schema({
-  apiKey: String,
-});
-
-module.exports = mongoose.model('Api', ApiSchema);
--- src/routes/api.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import { getApiKey } from '../controllers/api.controller.js';
-
-router.get('/', (getApiKey));
-
-module.exports = router;
